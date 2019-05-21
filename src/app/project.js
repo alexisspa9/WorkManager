df38947c646122a4ad633d164f06616f6b2f38ea
alexisspa9 <alexisspa9@gmail.com>
2019-05-22 01:32:25 +0300

    completed Project object and tests

--- src/app/project.js
+++ src/app/project.js
@@ -1,14 +1,34 @@
 const Todo = require("./todo.js");
+const Moment = require("moment");
 
-const Project = (name, total = 0, recurring = false) => {
+const Project = (name, total = 0, recurring = false, _start_date = Moment().format("MMM Do YY"), _due_date = Moment().add(1, 'days').format("MMM Do YY")) => {
     let _name = name;
     let complete = false;
     let _recurring = recurring;
-    let start_date = "";
-    let due_date = "";
+    let start_date = _start_date;
+    let due_date = _due_date;
     let paid = 0;
     let _total = total;
     let notes = "";
+    let todos = new Set();
+
+    const add_todo = (message) => {
+        const newTodo = Todo(message);
+        todos.add(newTodo);
+    }
+
+    const delete_todo = (todo) => {
+        todos.delete(todo);
+    }
+
+    const calculate_progress = () => {
+        const completed = [...todos].filter(todo => todo.show_values().complete === true);
+        let progress = 0;
+        if (todos.size > 0) {
+        progress = (parseFloat(completed.length) / parseFloat(todos.size)) * 100;
+        }
+        return progress;
+    }
 
     const show_values = () => {
         const values = {
@@ -19,7 +39,8 @@ const Project = (name, total = 0, recurring = false) => {
             dueDate: due_date,
             paid: paid,
             total: _total,
-            notes: notes
+            notes: notes,
+            todos: todos
         }
         return values;
     };
@@ -50,7 +71,10 @@ const Project = (name, total = 0, recurring = false) => {
         toggle_complete,
         set_total,
         set_paid,
-        set_notes
+        set_notes,
+        add_todo,
+        delete_todo,
+        calculate_progress
 
     }
 }
--- src/app/project.test.js
+++ src/app/project.test.js
@@ -1,4 +1,6 @@
 const Project = require('./project');
+const Moment = require("moment");
+
 
 
 const myproject = Project('newproject', 250);
@@ -9,11 +11,12 @@ it('show_values() returns project values', () => {
         name: 'newproject',
         complete: false,
         recurring: false,
-        startDate: "",
-        dueDate: "",
+        startDate: Moment().format("MMM Do YY"),
+        dueDate: Moment().add(1, 'days').format("MMM Do YY"),
         paid: 0,
         total: 250,
-        notes: ""
+        notes: "",
+        todos: new Set()
     });
 });
 
@@ -44,11 +47,39 @@ it('set_notes(val) set the notes value to the input value', () => {
 });
 
 
+it('add_todo(message) adds todo object to todos set', () => {
+    myproject.add_todo("this is a todo");
+    let thetodo = [...myproject.show_values().todos].filter(todo => todo.show_values().text === "this is a todo" )
+    expect(thetodo.length).toEqual(1);
+});
+
+it('add_todo(message) adds a second todo object to todos set', () => {
+    myproject.add_todo("this is another todo");
+    let theotherdo = [...myproject.show_values().todos].filter(todo => todo.show_values().text === "this is another todo")
+    expect(theotherdo.length).toEqual(1);
+    expect(myproject.show_values().todos.size).toEqual(2)
+});
+
+it('delete_todo(todo) deletes a todo from the set of todos', () => {
+    [...myproject.show_values().todos].forEach(todo => {
+        if (todo.show_values().text == "this is another todo") {
+            myproject.delete_todo(todo)
+        }
+    });
+    expect(myproject.show_values().todos.size).toEqual(1)
+});
+
+it('calculate_progress() returns project progress', () => {
+    myproject.add_todo("this is another todo");
+    myproject.add_todo("this is another todo");
+    myproject.add_todo("this is another todo");
+    [...myproject.show_values().todos].forEach(todo => {
+        if (todo.show_values().text == "this is another todo") {
+            todo.toggle_complete();
+        }
+    });
+    expect(myproject.calculate_progress()).toEqual(75);
+});
+
+
 //has value recurring or one time
-//has start date
-//has due date
-//has todos
-//can add a todo
-//can delete a todo
-//can edit a todo
-//can calculate progress
