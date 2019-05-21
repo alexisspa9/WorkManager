7e8e8a782f1f7b4a43af4fbaab727bb5feecb60d
alexisspa9 <alexisspa9@gmail.com>
2019-05-21 21:12:21 +0300

    progress Project factory and tests

--- src/app/project.js
+++ src/app/project.js
@@ -1,20 +1,56 @@
 const Todo = require("./todo.js");
 
-const Project = (name) => {
-    _name = name;
+const Project = (name, total = 0, recurring = false) => {
+    let _name = name;
+    let complete = false;
+    let _recurring = recurring;
+    let start_date = "";
+    let due_date = "";
+    let paid = 0;
+    let _total = total;
+    let notes = "";
 
-    const show_name = () => {
-        return _name;
-    }
+    const show_values = () => {
+        const values = {
+            name: _name,
+            complete: complete,
+            recurring: _recurring,
+            startDate: start_date,
+            dueDate: due_date,
+            paid: paid,
+            total: _total,
+            notes: notes
+        }
+        return values;
+    };
 
     const edit_name = (newname) => {
         _name = newname
     }
 
-    return {
+    const toggle_complete = () => {
+        complete = !complete
+    }
 
-        show_name,
-        edit_name
+    const set_total = (val) => {
+        _total = val;
+    }
+
+    const set_paid = (val) => {
+        paid = val;
+    }
+
+    const set_notes = (val) => {
+        notes = val;
+    }
+
+    return {
+        show_values,
+        edit_name,
+        toggle_complete,
+        set_total,
+        set_paid,
+        set_notes
 
     }
 }
--- src/app/project.test.js
+++ src/app/project.test.js
@@ -1,26 +1,52 @@
 const Project = require('./project');
 
 
-const myproject = Project('newproject');
+const myproject = Project('newproject', 250);
 
 
-it('show_name() returns the name of the project', () => {
-    expect(myproject.show_name()).toEqual('newproject');
+it('show_values() returns project values', () => {
+    expect(myproject.show_values()).toEqual({
+        name: 'newproject',
+        complete: false,
+        recurring: false,
+        startDate: "",
+        dueDate: "",
+        paid: 0,
+        total: 250,
+        notes: ""
+    });
 });
 
 it('edit_name(newname) edits the name of the project to the new name', () => {
     myproject.edit_name('newname');
-    expect(myproject.show_name()).toEqual('newname');
+    expect(myproject.show_values().name).toEqual('newname');
+});
+
+it('toggle_complete() toggles complete value', () => {
+    myproject.toggle_complete();
+    expect(myproject.show_values().complete).toBe(true);
+});
+
+it('set_total(val) set the total amount to the input value', () => {
+    myproject.set_total(500);
+    expect(myproject.show_values().total).toBe(500);
+});
+
+it('set_paid(val) set the paid amount to the input value', () => {
+    myproject.set_paid(200);
+    expect(myproject.show_values().paid).toBe(200);
 });
 
 
-//has value complete
+it('set_notes(val) set the notes value to the input value', () => {
+    myproject.set_notes("this is a test");
+    expect(myproject.show_values().notes).toEqual("this is a test");
+});
+
+
 //has value recurring or one time
 //has start date
 //has due date
-//has paid property
-//has total property
-//has notes property
 //has todos
 //can add a todo
 //can delete a todo
